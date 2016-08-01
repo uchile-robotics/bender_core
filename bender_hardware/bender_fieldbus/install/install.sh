f08aef8b50720ad975581505eea0b3c4cae11ad2
Rodrigo Muñoz <rorro.mr@gmail.com>
2016-08-01 17:08:33 -0400

    bender_fieldbus install script update

--- bender_hardware/bender_fieldbus/install/install.sh
+++ bender_hardware/bender_fieldbus/install/install.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/bash
+
+# Color
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+reset=$(tput sgr0)
+bold=$(tput bold)
 
 # Line tittle
-installer="[bender_fieldbus]:"
+installer="${bold}[bender_fieldbus]:${reset}"
 
 #  - - - - - - - - - Install Rules - - - - - - - - - - - 
 echo "$installer Installing udev rules"
@@ -15,6 +22,24 @@ echo "$installer Add user to dialout group"
 sudo usermod -a -G dialout "$USER"
 # Port permissions
 echo "$installer Add ports permissions"
-#sudo chmod a+rw /dev/bender/l_port
-#sudo chmod a+rw /dev/bender/r_port
-echo "$installer The computer must be restarted in order to complete the installation"
+if [ -f /dev/bender/l_port ];
+then
+   echo "Port /dev/bender/l_port connected."
+   echo "Add ports permissions."
+   sudo chmod a+rw /dev/bender/l_port
+else
+   echo "$installer ${red}Port /dev/bender/l_port NOT connected.${reset}"
+   echo "$installer ${yellow}You must add permissions using $ sudo chmod a+rw /dev/bender/l_port${reset}"
+fi
+
+if [ -f /dev/bender/r_port ];
+then
+   echo "$installer Port /dev/bender/r_port connected."
+   echo "$installer Add ports permissions."
+   sudo chmod a+rw /dev/bender/r_port
+else
+   echo "$installer ${red}Port /dev/bender/r_port NOT connected.${reset}"
+   echo "$installer ${yellow}You must add permissions using $ sudo chmod a+rw /dev/bender/r_port${reset}"
+fi
+
+echo "$installer ${yellow}The computer must be restarted in order to complete the installation${reset}"
