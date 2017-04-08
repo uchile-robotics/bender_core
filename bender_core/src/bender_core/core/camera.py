eff261f153a246de0883beaab3bed06ebf7ce234
Matías Pavez <matias.pavez.b@gmail.com>
2017-04-08 17:22:29 -0300

    camera skill .. cartoon effect

--- bender_core/src/bender_core/core/camera.py
+++ bender_core/src/bender_core/core/camera.py
@@ -64,6 +64,44 @@ class CameraSkill(RobotSkill):
             return False
         return self.cvimg_to_file(image, filename)
 
+    def apply_effect_cartoon(self, cv_image):
+
+        num_down = 2  # number of downsampling steps
+        num_bilateral = 7  # number of bilateral filtering steps
+
+        # downsample image using Gaussian pyramid
+        img_color = cv_image
+        for _ in xrange(num_down):
+            img_color = cv2.pyrDown(img_color)
+
+        # repeatedly apply small bilateral filter instead of
+        # applying one large filter
+        for _ in xrange(num_bilateral):
+            img_color = cv2.bilateralFilter(img_color, d=9,
+                                            sigmaColor=9,
+                                            sigmaSpace=7)
+
+        # upsample image to original size
+        for _ in xrange(num_down):
+            img_color = cv2.pyrUp(img_color)
+
+        # convert to grayscale and apply median blur
+        img_gray = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
+        img_blur = cv2.medianBlur(img_gray, 7)
+
+        # detect and enhance edges
+        img_edge = cv2.adaptiveThreshold(img_blur, 255,
+                                         cv2.ADAPTIVE_THRESH_MEAN_C,
+                                         cv2.THRESH_BINARY,
+                                         blockSize=9,
+                                         C=2)
+
+        # convert back to color, bit-AND with color image
+        img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
+        img_cartoon = cv2.bitwise_and(img_color, img_edge)
+
+        return img_cartoon
+
     def save_last_img(self, filename):
         msgimage = self.get_last_image()
         if msgimage is None:
