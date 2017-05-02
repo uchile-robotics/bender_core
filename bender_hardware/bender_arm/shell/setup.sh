9505f605d5680983c81f2fa73932725a05a4dacc
Bender <bender.devel@gmail.com>
2017-05-02 19:56:03 -0300

    update hwcheck for head, larm and rarm

---
+++ bender_hardware/bender_arm/shell/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+alias bender_hwcheck_l_arm="rosrun bender_arm hw_check_l_arm.py"
+alias bender_hwcheck_r_arm="rosrun bender_arm hw_check_r_arm.py"
---
+++ bender_hardware/bender_head/scripts/hwcheck/hw_check.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import os
+import sys
+from bender_utils.syscheck import SystemCheck, SystemCheckTask, FileCheckTask
+from bender_fieldbus.check import DynamixelCheck
+
+def head_check():
+    head = SystemCheck("left arm")
+    head.add_child(FileCheckTask('/dev/bender/l_port'))
+    head.add_child(DynamixelCheck('/dev/bender/l_port', 200000, [9]))
+    return head.check()
+
+if __name__ == "__main__":
+    if head_check():
+        sys.exit(0)
+    sys.exit(1)
