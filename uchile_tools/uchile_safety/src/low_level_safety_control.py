ddb157313de38023825255d338aedb69d692d050
Matías Pavez <matias.pavez.b@gmail.com>
2017-05-15 22:48:19 -0400

    migrating.. uchile_safety

--- uchile_tools/uchile_safety/src/low_level_safety_control.py
+++ uchile_tools/uchile_safety/src/low_level_safety_control.py
@@ -7,7 +7,7 @@ from math import sin, cos, atan2, pi, sqrt, pow as mpow
 import numpy
 import roslib
 import tf
-roslib.load_manifest("bender_safety")
+roslib.load_manifest("uchile_safety")
 
 from threading import Thread, Lock
 from geometry_msgs.msg import Twist, PoseStamped
