af2d28ff5ee50b78d83636f1df550b65646e7fe0
Luz Martínez <lmartine@gmail.com>
2017-05-18 19:06:15 -0400

    merged feat-calibration to develop

---
+++ bender_hardware/bender_calibrate/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['bender_calibrate'],
+    scripts=['src/cameracalibrator.py','src/cameracheck.py','src/tarfile_calibration.py'],
+    # obs: this should point to my_script_1.py ... (not folders)
+    #scripts=['src/bender_macros'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
