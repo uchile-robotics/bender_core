a6e30bacd07388f431497177a073c044d4702069
Rodrigo Munoz <rorro.mr@gmail.com>
2017-06-25 21:40:25 -0400

    Remove librobotis from check

--- bender_fieldbus/src/bender_fieldbus/check.py
+++ bender_fieldbus/src/bender_fieldbus/check.py
@@ -3,10 +3,10 @@
 """This module implements Hardware check methods"""
 
 from __future__ import print_function
-import os
-import sys
 from lib_robotis import USB2Dynamixel_Device, Robotis_Servo
 from uchile_util.syscheck import SystemCheck, SystemCheckTask, FileCheckTask
+from dynamixel_driver.dynamixel_io import DynamixelIO
+from time import sleep
 
 __author__ = 'Rodrigo Muñoz'
 __email__ = 'rorro.mr@gmail.com'
@@ -23,21 +23,28 @@ class DynamixelCheck(SystemCheckTask):
         SystemCheck.print_high("Target device  : " + self.device, 1)
         SystemCheck.print_high("Target baudrate: %d" % self.baud, 1)
         try:
-            SystemCheck.print_info("Creating USB2Dynamixel interface ... ", 1)
-            dxl = USB2Dynamixel_Device(self.device, self.baud)
+            SystemCheck.print_info("Opening serial port... ", 1)
+            dxl = DynamixelIO(self.device, self.baud)
+            # Send zeros for restart all state machines on servos
+            dxl.__write_serial([0]*20)
+            sleep(0.1)
+            dxl.__write_serial([0]*20)
+
         except:
-            SystemCheck.print_error('Error opening: {} [baud {}]\t\t[FAIL]'
+            SystemCheck.print_error("Error opening: {} [baud {}]\t\t[FAIL]"
                 .format(self.device, self.baud))
             return False
 
         SystemCheck.print_info("Checking ids  : %s" % self.ids, 1)
-        dxl.servo_dev.setTimeout(0.1)
         result = True
         for i in self.ids:
             try:
-                Robotis_Servo(dxl, i)
-                SystemCheck.print_ok('motor id: {}'.format(i), 2)
+                result = dxl.ping(i)
+                if result:
+                    SystemCheck.print_ok("Motor id: {}".format(i), 2)
+                else:
+                    raise RuntimeError("Motor id:{} not found".format(i))
             except:
-                SystemCheck.print_error('motor id: {} not found'.format(i), 2)
+                SystemCheck.print_error("Motor id: {} not found".format(i), 2)
                 result = False
         return result
