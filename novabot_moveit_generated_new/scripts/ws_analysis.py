6d2fe129c5b4c28096809c9feee3de493a2d341a
sparraf <sebastian.parra.flores@gmail.com>
2017-11-03 17:11:44 -0300

    added workspace parameters

--- novabot_moveit_generated_new/scripts/ws_analysis.py
+++ novabot_moveit_generated_new/scripts/ws_analysis.py
@@ -6,6 +6,7 @@ __author__ = 'Giovanni Pais'
 __email__ = 'giom.pais@gmail.com'
 
 import rospy
+import csv
 from moveit_python import MoveGroupInterface
 
 from geometry_msgs.msg import Pose, PoseStamped, Point
@@ -29,14 +30,14 @@ def Move(mgi, tip_link, goal):
     else:
         rospy.logerr('Planning Failed')
         rospy.logerr('Error code: {0}'.format(result.error_code.val))
-        return -1
+        return result.error_code.val#-1
 
 
 
 
 def main():
         
-    mgi = MoveGroupInterface("nova_arm", "r_arm_support_link",None,False)  #"bender/l_shoulder_pitch_link"
+    mgi = MoveGroupInterface("nova_arm", "r_arm_support_link",None,True)  #"bender/l_shoulder_pitch_link"
     tip_link="r_wrist_pitch_link"
     joint_names=['r_shoulder_roll_joint','r_shoulder_pitch_joint',
                 'r_elbow_pitch_joint','r_elbow_roll_joint',
@@ -56,31 +57,37 @@ def main():
     goal.pose.position.x, goal.pose.position.y, goal.pose.position.z = 0.2, 0.1, 0.1
     #simple_orientation = Quaternion(0.0,-0.70710678,0.0,0.70710678)
     #Orientation
-    goal.pose.orientation.x, goal.pose.orientation.y, goal.pose.orientation.z, goal.pose.orientation.w = 0.0, 0.0, 0.0, 1.0 #-0.0148,-0.733 , 0.0, 0.678
+    goal.pose.orientation.x, goal.pose.orientation.y, goal.pose.orientation.z, goal.pose.orientation.w = 0.05726, 0.5419 , -0.66364, 0.51251 #-0.0148,-0.733 , 0.0, 0.678
     goal.header.frame_id="r_arm_support_link"
     #---------------------------------------------------------------------------------------------------------------------------------------------------
     #------------------------------------------------Iterarion Params-----------------------------------------------------------------------------------
-    dl=0.1 #precision en metros
-    Xmin=0.0  #0.1
-    Xmax=0.1  #0.5
-    Ymin=-0.1 #0.0
-    Ymax=0.0  #0.3
-    Zmin=0.5  #0.6
-    Zmax=0.6  #0.8
+    dl=0.01 #precision en metros (0.03)
+    Xmin=0.0  #-0.05
+    Xmax=0.8  #0.8
+    Ymin=-0.4 #-0.4
+    Ymax=0.4  #0.05
+    Zmin=0.0  #0.55
+    Zmax=1.2  #0.85  
+    # ~17 horas worst case scenario
     #---------------------------------------------------------------------------------------------------------------------------------------------------
+    workspace = [] #matrix for saving coordinates and error codes
     for i in my_range(Xmin,Xmax,dl):
         for j in my_range(Ymin,Ymax,dl):
             for k in my_range(Zmin,Zmax,dl):
                 goal.pose.position.x, goal.pose.position.y, goal.pose.position.z = i,j,k
-                if(Move(mgi,tip_link,goal)==1):
+                error_code = Move(mgi,tip_link,goal)
+                if(error_code == 1):#Move(mgi,tip_link,goal)==1):
                     succ_plan+=1
                 count +=1
+                workspace.append([error_code,i,j,k])
 
     percentage=(succ_plan/count)*100
     rospy.loginfo('succeeded plannings: {0}/{1}  ({2}%)'.format(succ_plan,count,percentage))
     rospy.loginfo('TEST ENDED')
+    with open('workspace.csv','wb') as f:
+        writer = csv.writer(f)
+        writer.writerows(workspace)
     ################################
-
 
 if __name__ == '__main__':
     rospy.init_node('Nova_ws_analysis')
