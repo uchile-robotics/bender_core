b8a973edec079aab4196bd8b917a41163fdd336b
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-05-26 22:55:08 -0400

    (FG): added base parameter reading

--- bender_base/README.md
+++
@@ -1,8 +0,0 @@
-# rosaria2
-ROS2 version of the legacy rosaria (https://github.com/amor-ros-pkg/rosaria) package
-
-This driver supports several well-known mobile robot platforms from Adept Robotics, including the Pioneer P3-DX2.
-
-This version of the driver was ported to ROS Jazzy (a ROS 2 distribution) and tested successfully in this ROS distro by Rui P. Rocha (ISR - University of Coimbra, [rprocha@isr.uc.pt](mailto:rprocha@isr.uc.pt)) on Sep. 2024.
-
-For information on how to use RosAria, see http://wiki.ros.org/ROSARIA, especially <http://wiki.ros.org/ROSARIA/Tutorials/How to use ROSARIA>.
--- bender_base/launch/rosaria2.launch.py
+++ bender_base/launch/rosaria2.launch.py
@@ -2,42 +2,25 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
+import os
 def launch_setup(context, *args, **kwargs):
     # Get launch configurations
-    namespace = context.launch_configurations['namespace']
-    serial_port = context.launch_configurations['serial_port']
-    remap_cmd_vel = context.launch_configurations['remap_cmd_vel'].lower() in ['true', '1', 'yes']
-    sonar_enabled = context.launch_configurations['sonar_enabled'].lower() in ['true', '1', 'yes']
-    publish_motors_state = context.launch_configurations['publish_motors_state'].lower() in ['true', '1', 'yes']
-
-    # Create namespace-based prefix
-    prefix = f'{namespace}/' if namespace != '' else ''
-
-    # Remapping logic
-    from_topic = 'cmd_vel'
-    to_topic = 'cmd_vel_not_stamped' if remap_cmd_vel else 'cmd_vel'
+    pkg = 'bender_base'
+    parameters = [os.path.join(
+        get_package_share_directory(pkg),
+        'params',
+        'pioneer.yaml'
+    )]
 
     # Create and return the Node
     return [
         Node(
-            namespace=namespace,
+            namespace='',
             package='rosaria2',
             executable='rosaria2_node',
             name='pioneer_p3dx_driver',
-            parameters=[{
-                'tf_prefix': namespace,
-                'odom_frame_id': 'odom',
-                'base_frame_id': prefix + 'base_link',
-                'bumper_frame_id': 'bumper',
-                'sonar_frame_id': prefix + 'sonar',
-                'serial_port': serial_port,
-                'sonar_enabled': sonar_enabled,
-                'publish_sonar': sonar_enabled,
-                'publish_sonar_pointcloud2': sonar_enabled,
-                'publish_motors_state': publish_motors_state,
-            }],
-            remappings=[(from_topic, to_topic)],
+            parameters=[parameters],
         )
     ]
 
