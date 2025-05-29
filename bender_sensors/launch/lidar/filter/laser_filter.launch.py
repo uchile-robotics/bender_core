e3448ffe0345455399d7bf0f2754eb678731c4a4
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-05-29 13:18:01 -0400

    (FG): modified laser filter launch

--- bender_sensors/launch/lidar/filter/laser_filter.launch.py
+++ bender_sensors/launch/lidar/filter/laser_filter.launch.py
@@ -1,32 +1,28 @@
 from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
-import os
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
-    pkg = 'bender_sensors'
-    parameters = os.path.join(
-        get_package_share_directory(pkg),
-        'params',
-        'lidar',
-        'filter',
-        'scan_filter_chain.yaml'
-    )
-
-
+    
+    params = PathJoinSubstitution([
+        get_package_share_directory("bender_sensors"),
+        "params",
+        "lidar",
+        "filter", 
+        "scan_filter_chain.yaml"
+    ])
     return LaunchDescription([
         Node(
-            package='laser_filters',
-            executable='scan_to_scan_filter_chain',
-            name='laser_filter_node',
-            parameters=[
-                {'scan_topic': '/scan_raw'},
-                parameters
-            ],
-
+            package="laser_filters",
+            executable="scan_to_scan_filter_chain",
             remappings=[
-                ('scan', '/scan_raw'),  # entrada
-                ('scan_filtered', '/scan')  # salida
+                ("scan", "/scan_raw"),
+                ("scan_filtered", "/scan"),
+            ],
+            parameters=[
+                params
             ],
         )
     ])
