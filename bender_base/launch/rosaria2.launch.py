5a0de453a1a5dd11ff3876e1c77139d0fe2f73a6
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-06-04 11:11:11 -0400

    (FG): Modified parameter files

--- bender_base/launch/rosaria2.launch.py
+++ bender_base/launch/rosaria2.launch.py
@@ -4,7 +4,7 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
-def launch_setup(context, *args, **kwargs):
+def generate_launch_description(*args, **kwargs):
     # Get launch configurations
     pkg = 'bender_base'
     parameters = [os.path.join(
@@ -14,22 +14,12 @@ def launch_setup(context, *args, **kwargs):
     )]
 
     # Create and return the Node
-    return [
+    return LaunchDescription([
         Node(
             namespace='',
             package='rosaria2',
             executable='rosaria2_node',
-            name='pioneer_p3dx_driver',
+            name='rosaria2',
             parameters=[parameters],
         )
-    ]
-
-def generate_launch_description():
-    return LaunchDescription([
-        DeclareLaunchArgument('namespace', default_value=''),
-        DeclareLaunchArgument('serial_port', default_value='/dev/pioneer'),
-        DeclareLaunchArgument('sonar_enabled', default_value='false'),
-        DeclareLaunchArgument('publish_motors_state', default_value='true'),
-        DeclareLaunchArgument('remap_cmd_vel', default_value='false'),
-        OpaqueFunction(function=launch_setup),
     ])
