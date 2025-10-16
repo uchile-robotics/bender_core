75a8f4f3dd0b8d59f11b8653cb46e9817a8cb993
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-10-16 10:56:38 -0300

    (FG): joy fix

--- bender_joy/launch/joystick.launch.py
+++ bender_joy/launch/joystick.launch.py
@@ -12,8 +12,9 @@ def generate_launch_description():
     joy_params = os.path.join(get_package_share_directory('bender_joy'),'params','joystick.yaml')
 
     joy_node = Node(
-            package='joy',
-            executable='joy_node',
+            package='joy_linux',
+            executable='joy_linux_node',
+            name='joy_linux_node',
             parameters=[joy_params, {'use_sim_time': use_sim_time}],
          )
 
